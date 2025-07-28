2bcb2ab75a5b4f7ebb26a32be4c6c6644cb6c5d6
Herbepin Emmanuel <150336611+E-Herbepin@users.noreply.github.com>
2025-07-28 23:59:58 +0200

    Update run_project.R

--- run_project.R
+++ run_project.R
@@ -10,20 +10,13 @@ if (!dir.exists(processed_data_dir)) {
   dir.create(processed_data_dir, recursive = TRUE)
   dir.create(docs_dir, recursive = TRUE)
   source(here(scripts_dir, "data_preparation.R"))
-  rm(list = setdiff(ls(), c("df_indicators", "df_demo", "df_status", "ls_tb_corres")))
   cat("Data prepared.\n")
-} else {
-  load(here(processed_data_dir, "correspondence_tables.RData"))
-  read.csv2(here(processed_data_dir, "df_indicators.csv"))
-  read.csv2(here(processed_data_dir, "demographic_indicators.csv"))
-  read.csv2(here(processed_data_dir, "status.csv"))
-  cat("Data loaded\n")
 }
 
-# Step 3: Render final report
+# Step 2: Render final report
 rmarkdown::render(
   input = file.path(scripts_dir, "report.Rmd"),
-  output_format = "pdf_document",
+  output_format = "html_document",
   output_file = file.path(docs_dir, "final_report.pdf")
 )
 cat("Report rendered successfully.\n")
